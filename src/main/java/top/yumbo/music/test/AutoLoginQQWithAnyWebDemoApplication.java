651631d9431e79a28ee8828b0df73b94cf30eb94
angel <1015770492@qq.com>
2021-01-17 01:54:01 +0800

    springboot应用，实现qq自动登录返回cookie

---
+++ src/main/java/top/yumbo/music/test/AutoLoginQQWithAnyWebDemoApplication.java
@@ -0,0 +1,20 @@
+package top.yumbo.music.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class AutoLoginQQWithAnyWebDemoApplication {
+
+
+    /**
+     * 代码比较随意，自行增改调整
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(AutoLoginQQWithAnyWebDemoApplication.class, args);
+    }
+
+
+}
--- src/main/java/top/yumbo/music/test/controller/LoginController.java
+++ src/main/java/top/yumbo/music/test/controller/LoginController.java
@@ -1,102 +1,3 @@
-windows和linux都差不多，只要环境搭建好，然后再执行程序就能实现快速登录
-
-### 想要见效果的看这篇博客有gif演示效果：
-[java+selenium-java 实现qq自动登录并获取cookie](https://blog.csdn.net/qq_41813208/article/details/112646537)
-### 完整的环境搭建过程
-
-### 第一步给Linux服务器安装google-chrome（谷歌浏览器）
-Centos操作系统的使用下面这个
-
-下载rpm包
-```bash
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-```
-安装依赖
-```bash
-sudo yum install -y lsb
-```
-进行安装
-```bash
-sudo yum localinstall google-chrome-stable_current_x86_64.rpm
-```
-***
-Ubuntu用户的使用下面的
-下面命令建议不使用root身份进行下载
-因为可能会报权限问题：
-N: Download is performed unsandboxed as root as file '/root/google-chrome-stable_current_amd64.deb' couldn't be accessed by user '_apt'. - pkgAcquire::Run (13: Permission denied)
-
-
-***
-下载deb包
-
-```bash
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-```
-安装deb包 
-
-```bash
-sudo apt install -y ./google-chrome-stable_current_amd64.deb
-```
-完成后Centos和Ubuntu都会多一个命令
-
-```bash
-google-chrome
-```
-
-### 第二步，下载chromedriver 它的作用是操作google-chrome浏览器，也就是通过它来对谷歌浏览器操作的
-下载地址：[https://npm.taobao.org/mirrors/chromedriver/](https://npm.taobao.org/mirrors/chromedriver/)
-
-随便选一个吧，我选的是这个版本的：[https://npm.taobao.org/mirrors/chromedriver/86.0.4240.22/](https://npm.taobao.org/mirrors/chromedriver/86.0.4240.22/)
-
-![在这里插入图片描述](https://img-blog.csdnimg.cn/20210117005250313.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3FxXzQxODEzMjA4,size_16,color_FFFFFF,t_70)
-将它上传到linux服务器上，通过FTP工具
-ftp工具 例如：[FileZila下载页面](https://www.filezilla.cn/download/client)选择适合自己的
-或者xftp
-ftp 工具 和 chromedriver也可以通过下面的csdn进行下载，我将它打包成了一个zip包。也可以选择前面发的链接自行下载接口
-[https://download.csdn.net/download/qq_41813208/14503894](https://download.csdn.net/download/qq_41813208/14503894)
-
-将他上传到一个目录下，例如`/home/angel`
-![在这里插入图片描述](https://img-blog.csdnimg.cn/20210117010516450.png)
-#### 授予执行权限
-执行完后就会是绿色的提示。
-```bash
-chmod 777 chromedriver
-```
-
-### 第三步、下载jar包，或者去github自行打成jar包（不放心的可以看下源码，很简单的代码）
-java环境需要先弄好
-Ubuntu用：`apt install -y openjdk-14-jre-headless`进行安装即可
-Centos用：`yum install -y java-11-openjdk-devel.x86_64`安装即可（如果需要配置环境变量自行配置即可）
-
-是一个springboot项目，端口是：7000，可以通过运行参数修改
-
-```bash
-java -jar -Dwebdriver.chrome.driver=/home/angel/chromedriver -Dserver.port=7000 autoLoginQQWithAnyWeb-1.0.jar
-```
-
-### 第四步发送请求得到cookie信息
-`get/post`都支持，接收`json`/`传参数`
-如下
-接口地址：`/login`
-在线地址：`http://yumbo.top:7000/login`
-
-下面是qq音乐的登录地址（qq音乐它不会跳转到后面的那个界面，需要通过抓包分析）
-替换下面的username和password即可完成登录获取登录qq音乐后的cookie
-方便复制：下面的这个模板只适合qq音乐，其它网站则根据后面参数补充的那个页面直接复制url代替这里参数的url即可
-```
-http://yumbo.top:7000/login?url=https://graph.qq.com/oauth2.0/authorize?response_type=code%26state=state%26client_id=100497308%26redirect_uri=https://y.qq.com/portal/wx_redirect.html?login_type=1%26surl=https%3A%2F%2Fy.qq.com%2Fportal%2Fradio.html%23stat%3Dy_new.top.pop.logout%26stat%3Dy_new.top.pop.logout%26stat%3Dy_new.top.pop.logout%26stat%3Dy_new.top.pop.logout%26stat%3Dy_new.top.pop.logout%26use_customer_cb=0&username=qq号&password=qq密码&format=2
-```
-
-![在这里插入图片描述](https://img-blog.csdnimg.cn/20210117013935192.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3FxXzQxODEzMjA4,size_16,color_FFFFFF,t_70)
-#### 参数补充说明
-url 是登录qq的那个页面，复制登录界面的url 传入即可
-![在这里插入图片描述](https://img-blog.csdnimg.cn/20210117014505688.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3FxXzQxODEzMjA4,size_16,color_FFFFFF,t_70)
-
-
-
-
-#### 处理请求的Controller源码
-```java
 package top.yumbo.music.test.controller;
 
 import com.alibaba.fastjson.JSONObject;
@@ -108,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.Set;
 
 @RestController
@@ -202,4 +104,3 @@ public class LoginController {
     }
 
 }
-```
